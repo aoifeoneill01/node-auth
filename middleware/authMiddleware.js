8a11f9d71ae04ab2e4fb1f7f2e108505c15d05d1
aoifeoneill01 <aoife22oneill@gmail.com>
2021-04-26 09:27:53 +0100

    all files

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,42 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+const requireAuth = (req, res, next) => {
+    const token = req.cookies.jwt;
+
+    // Check token exists
+    if(token){
+        jwt.verify(token, '<token secret name>', (err, decodedToken) => {
+            if(err){
+                console.log(err.message);
+                res.redirect('/login')
+            }
+            else {
+                next();
+            }
+        })
+
+    } else {
+    res.redirect('/login');
+    }
+}
+
+// Check current user
+const checkUser = (req, res, next) => {
+    const token = req.cookies.jwt;
+
+    if(token){
+        jwt.verify(token, '<token secret name>', async (err, decodedToken) => {
+            if(err){
+                console.log(err.message);
+                next();
+            }
+            else {
+                let user = await User.findById(decodedToken.id);
+                next();
+            }
+        })
+    }
+}
+
+module.exports = { requireAuth, checkUser };
